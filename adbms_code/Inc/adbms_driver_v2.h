c33c2981064aef22076ad7e7fc2ba17dfac34749
JasperZihanWeng <jasperweng@outlook.com>
2026-04-15 19:44:14 -0500

    some initial documents

---
+++ adbms_code/Inc/adbms_driver_v2.h
@@ -0,0 +1,222 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+//taken out because we don't wanna call any hal commands
+//#include "stm32h7xx_hal.h"
+
+#include "adbms_cmd.h"
+#include "bms_system_prams.h"
+
+
+#define SPI_TIME_OUT HAL_MAX_DELAY
+
+
+//this struct stores all the parsed bit values of commands
+typedef struct
+{
+    //config A and config B
+    uint8_t cfg_a[NUM_CHIPS * DATA_LEN];
+    uint8_t cfg_b[NUM_CHIPS * DATA_LEN];
+
+    // 6830 commands that also work for 2950
+    uint16_t adcv;
+    uint16_t adsv;
+    uint16_t adax;
+    uint16_t adax2;
+
+    // 2950 specific command
+    uint16_t adv;
+
+} config_command_bits_;
+
+
+//this struct stores all spi related stuff
+typedef struct
+{
+    SPI_HandleTypeDef *hspi;
+    uint8_t spi_dataBuf[DATABUF_LEN];
+
+} SPI_data_;
+
+
+//this struct stores all struct of all the bit values coming back from reads
+typedef struct
+{
+    uint8_t cell[NUM_CHIPS * CELL_REG_GRP * DATA_LEN];
+    uint8_t scell[NUM_CHIPS * CELL_REG_GRP * DATA_LEN];
+    uint8_t aux[NUM_CHIPS * CELL_REG_GRP * DATA_LEN]; 
+    uint8_t shunt_temp[NUM_CHIPS * DATA_LEN];
+
+} raw_read_return_values_;
+
+typedef struct
+{
+    bool read_voltage_pec_failure;
+    bool read_temp_pec_failure;
+    bool read_open_wire_pec_failure;
+
+} read_failures_;
+
+
+
+//
+// This section is for commands that have options that can be changed
+//
+
+typedef struct
+{
+    uint8_t       refon   :1;
+    uint8_t       cth     :3;
+    uint8_t       flag_d  :8;
+    uint8_t       soakon  :1;
+    uint8_t       owrng   :1;
+    uint8_t       owa     :3;
+    uint16_t      gpo     :10;
+    uint8_t       snap    :1;
+    uint8_t       mute_st :1;
+    uint8_t       comm_bk :1;
+    uint8_t       fc      :3;
+}cfa6830_;
+
+
+typedef struct{
+    uint8_t       ocen    :1;
+    uint8_t       vs5     :1;
+    uint8_t       vs4     :1;
+    uint8_t       vs3     :1;
+    uint8_t       vs2     :2;
+    uint8_t       vs1     :2;
+    uint8_t       injtm   :1;
+    uint8_t       injecc  :1;
+    uint8_t       injts   :1;
+    uint8_t       injmon  :2;
+    uint8_t       injosc  :2;
+    uint8_t       soak    :3;
+    uint8_t       vs10    :1;
+    uint8_t       vs9     :1;
+    uint8_t       vs8     :1;
+    uint8_t       vs7     :1;
+    uint8_t       vs6     :1;
+    uint8_t       gpo6c   :2;
+    uint8_t       gpo5c   :1;
+    uint8_t       gpo4c   :1;
+    uint8_t       gpo3c   :1;
+    uint8_t       gpo2c   :1;
+    uint8_t       gpo1c   :1;
+    uint8_t       spi3w   :1;
+    uint8_t       gpio1fe :1;
+    uint8_t       gpo6od  :1;
+    uint8_t       gpo5od  :1;
+    uint8_t       gpo4od  :1;
+    uint8_t       gpo3od  :1;
+    uint8_t       gpo2od  :1;
+    uint8_t       gpo1od  :1;
+    uint8_t       vb2mux  :1;
+    uint8_t       vb1mux  :1;
+    uint8_t       snapst  :1;
+    uint8_t       refup   :1;
+    uint8_t       commbk  :1;
+    uint8_t       acci    :3;
+
+}cfa2950_;
+
+/* For ADBMS6830 config register structure */
+typedef struct
+{
+    uint16_t 	vuv     :12;
+    uint16_t 	vov     :12;
+    uint8_t 	dtmen   :1;
+    uint8_t 	dtrng   :1;
+    uint8_t 	dcto    :6;
+    uint16_t 	dcc     :16;
+}cfb6830_;
+
+typedef struct
+{
+    uint8_t     oc1th       :7;
+    uint8_t     oc2th       :7;
+    uint8_t     oc3th       :7;
+    uint8_t     ocdp        :1;
+    uint8_t     ocdgt       :2;
+    uint8_t     ocbx        :1; 
+    uint8_t     ocax        :1;
+    uint8_t     ocmode      :2;
+    uint8_t     oc3gc       :1;
+    uint8_t     oc2gc       :1;
+    uint8_t     oc1gc       :1;
+    uint8_t     ocod        :1;
+    uint8_t     gpio4c      :1;
+    uint8_t     gpio3c      :1;
+    uint8_t     gpio2c      :1;
+    uint8_t     gpio1c      :1;
+    uint8_t     gpio2eoc    :1;
+    uint8_t     diagsel     :3;
+
+}cfb2950_;
+
+typedef struct
+{
+    uint8_t rd    :1;
+    uint8_t cont  :1;
+    uint8_t dcp   :1;
+    uint8_t rstf  :1;
+    uint8_t ow    :2;
+}adcv_;
+
+typedef struct
+{
+    uint8_t cont  :1;
+    uint8_t dcp   :1;
+    uint8_t ow    :2;
+}adsv_;
+
+typedef struct
+{
+    uint8_t ow    :1;
+    uint8_t pup   :1;
+    uint8_t ch    :5;
+}adax_;
+
+typedef struct
+{
+    uint8_t ch    :4;
+}adax2_;
+
+//this struct is for sending command adv for 2950 which starts v1adc and v2adc
+typedef struct
+{
+    uint8_t       ow      :2;
+    uint8_t       vch     :4;
+  
+}adv_;
+
+
+//driver functions
+uint16_t Pec15_Calc(uint8_t len, uint8_t *data);
+uint16_t Pec10_Calc(bool isRxCmd, int len, uint8_t *data);
+uint16_t Set_UnderOver_Voltage_Threshold(float voltage);
+
+
+//6830 configuration functions
+void ADBMS_Set_Config_A_6830(cfa6830_ *cfg_a, uint8_t *cfg_a_tx_buffer, uint8_t num_6830);
+void ADBMS_Set_Config_B_6830(cfb6830_ *cfg_b, uint8_t *cfg_b_tx_buffer, uint8_t num_6830);
+
+
+//2950 configuration functions
+void ADBMS_Set_Config_A_2950(cfa2950_* cfg_a2950, uint8_t* cfg_a_tx_buffer, uint8_t chip_position);
+void ADBMS_Set_Config_B_2950(cfb2950_* cfg_b2950, uint8_t* cfg_b_tx_buffer, uint8_t chip_position);
+
+//joint configuration functions
+void ADBMS_Set_ADCV(adcv_ adcv, uint16_t *adcv_cmd_buffer);
+void ADBMS_Set_ADSV(adsv_ adsv, uint16_t *adsv_cmd_buffer);
+void ADBMS_Set_ADAX(adax_ adax, uint16_t *adax_cmd_buffer);
+void ADBMS_Set_ADAX2(adax2_ adax2, uint16_t *adax2_cmd_buffer);
+void ADBMS_Set_ADV(adv_ adv, uint16_t* adv_cmd_buffer);
+
+//read write function
+void ADBMS_WakeUP_ICs();
+void ADBMS_Write_CMD(SPI_HandleTypeDef *hspi, uint16_t tx_cmd);
+void ADBMS_Write_Data(SPI_HandleTypeDef *hspi, uint16_t tx_cmd, uint8_t *data, uint8_t *spi_dataBuf);
+bool ADBMS_Read_Data(SPI_HandleTypeDef *hspi, uint16_t tx_cmd, uint8_t *data, uint8_t *spi_dataBuf);
---
+++ adbms_code/Inc/bms_system_prams.h
@@ -0,0 +1,101 @@
+#ifndef AD_SYSTEM_PARAMS_H
+#define AD_SYSTEM_PARAMS_H
+
+// Note for NFR25 we precharged across the negative contactor. 
+// This might change in future years, if so change which contactors are closed accoringly in the FSM 
+
+
+#define NUM_6830	2					/* Number of 6830 ICs */
+#define NUM_2950	1					/* Number of 2950 ICs*/
+#define NUM_CHIPS NUM_6830+NUM_2950 	/* Number of ICs 					*/
+#define NUM_VOLTAGES_EVEN_CHIP 12		/* Nmber of Cells in even number ICs starting at 0 ending at 8*/
+#define NUM_VOLTAGES_ODD_CHIP  11  	   	/* Nmber of Cells in odd number ICs starting at 1 ending at 9*/
+#define NUM_TEMPS_CHIP 10				/* Number of Temps to read per IC 	*/
+
+#define OVERVOLTAGE 4.2					/* Overvoltage Threshold 	*/
+#define UNDERVOLTAGE 1.5				/* Undervoltage Threshold 	*/
+#define OVERTEMP 90						/* Over Temp Threshold 		*/
+#define UNDERTEMP -40					/* Under Temp Threshold 	*/
+#define OVERCURRENT 135					/* Over Current Threshold 	*/
+
+#define PEC_FAILURE_THRESHOLD 10		/* Number of consecutive PEC failures that can happen before a pec fault occurs*/
+
+#define CB_THRESHOLD 0.01				/* Threshold Away From Lowest Cell to Start Cell Balancing when CB is enabled */
+#define CB_MIN_V_THRESHOLD 3			/* Cells under this threshold will not be balanced even when CB is enabled */
+
+#define ENABLE_PRINTF_DEBUG_COMMS 1	/* Flag to enable Printf debug comms */
+#define ENABLE_USB_COMMS 1				/* Flag to enable USB comms */
+
+#define NUM_CURRENT_OFFSET_CYCLES 10	/* Numbeer of Cycles to get ADC Offset */
+
+#define NUM_DATA_CAN_VOLTAGES_PER_MSG 7 // will break if this is changed. Based on how data can and DBC are set up
+#define NUM_DATA_CAN_VOLTAGE_MSGS (NUM_CHIPS * NUM_VOLTAGES_ODD_CHIP + ((NUM_CHIPS + 1)/2)) / NUM_DATA_CAN_VOLTAGES_PER_MSG
+#define NUM_DATA_CAN_TEMPS_PER_MSG 8    // will break if this is changed. Based on how data can and DBC are set up
+#define NUM_DATA_CAN_TEMP_MSGS (NUM_CHIPS * NUM_TEMPS_CHIP) / NUM_DATA_CAN_TEMPS_PER_MSG
+
+#define MAX_CHARGER_VOLTAGE 568
+#define MAX_CHARGER_CURRENT 3
+
+// BMS IC Parameters
+#define CELLS 	16														  /* Bms ic number of Cells                */
+#define CELL_REG_GRP 6													  /*Number of total voltage register groups (A-F)*/
+#define AUX		12														  /* Bms ic number of Aux             	   */
+#define AUX_GPIO 10
+#define AUX_REG_GRP 4
+#define VOLTAGES_REG_GRP 3												  /* Number of voltages a Register Group can represent*/
+#define CMD_LEN  2                                                        /* Number of CMD Bytes                   */
+#define DATA_LEN 6                                                        /* Number of Data Bytes in Register Groups*/
+#define PEC_LEN  2                                                        /* Number of PEC Bytes                   */
+#define DATABUF_LEN (CMD_LEN + PEC_LEN) + (DATA_LEN + PEC_LEN)*NUM_CHIPS  /* CMD Msg + PEC and (DATA + PEC) per IC */
+
+#define NUM_TIMERS 2    // will break if this is changed. This will be replaced by RTOS
+#define NUM_CURRENT_OFFSET_CYCLES 10
+
+#define INVERTER_VOLTAGE_THRESHOLD 0.9 
+#define CHARGER_VOLTAGE_THRESHOLD 0.75 
+
+// CAN TIMEOUTS (ms)
+#define VCU_CAN_TIMEOUT 5000
+#define INVERTER_CAN_TIMEOUT 5000
+#define CHARGER_CAN_TIMEOUT 5000
+
+//for SOC calculations
+#define CELL_CAPACITY 4.5
+#define PARALLEL 3
+#define SYSTEM_CAPACITY (CELL_CAPACITY * PARALLEL)
+
+//CAN IDs
+#define SOC_ID 0x150
+#define FAULT_ID 0x151
+#define STATUS_ID 0x152
+#define CHARGER_ID 0x1806E5F4
+#define VOLTAGES_ID 0x153
+#define TEMPS_ID 0x167
+
+// ENUMERATES
+enum bms_states
+{
+	Idle = 0,
+	Precharge = 1,
+	Active = 2,
+	Charging = 3, 
+	Fault = 4
+};
+
+enum vcu_states
+{
+	car_idle = 0,
+	car_precharge = 1,
+	car_neutral = 2,
+	car_drive = 3,
+	car_fault = 4
+};
+
+enum charging_states
+{
+	charger_setup = 0,
+	charger_precharge = 1,
+	charger_active = 2,
+};
+
+#endif // AD_SYSTEM_PARAMS_H
