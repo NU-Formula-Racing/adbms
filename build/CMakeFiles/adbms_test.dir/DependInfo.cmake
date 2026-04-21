abae07786264ac97451772c1e5e2ce4aedc98e33
JasperZihanWeng <jasperweng@outlook.com>
2026-04-21 16:21:43 -0500

    builds, but segmentation faults when run

---
+++ build/CMakeFiles/adbms_test.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/jasperweng/STM32Cube/adbms/adbms_code/Src/adbms_driver_v2.c" "CMakeFiles/adbms_test.dir/adbms_code/Src/adbms_driver_v2.c.o" "gcc" "CMakeFiles/adbms_test.dir/adbms_code/Src/adbms_driver_v2.c.o.d"
+  "/Users/jasperweng/STM32Cube/adbms/adbms_code/Src/adbms_interface_2950.c" "CMakeFiles/adbms_test.dir/adbms_code/Src/adbms_interface_2950.c.o" "gcc" "CMakeFiles/adbms_test.dir/adbms_code/Src/adbms_interface_2950.c.o.d"
+  "/Users/jasperweng/STM32Cube/adbms/adbms_code/Src/adbms_interface_6830.c" "CMakeFiles/adbms_test.dir/adbms_code/Src/adbms_interface_6830.c.o" "gcc" "CMakeFiles/adbms_test.dir/adbms_code/Src/adbms_interface_6830.c.o.d"
+  "/Users/jasperweng/STM32Cube/adbms/adbms_code/Src/adbms_interface_v2.c" "CMakeFiles/adbms_test.dir/adbms_code/Src/adbms_interface_v2.c.o" "gcc" "CMakeFiles/adbms_test.dir/adbms_code/Src/adbms_interface_v2.c.o.d"
+  "/Users/jasperweng/STM32Cube/adbms/adbms_code/Src/test.c" "CMakeFiles/adbms_test.dir/adbms_code/Src/test.c.o" "gcc" "CMakeFiles/adbms_test.dir/adbms_code/Src/test.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/adbms_test.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/adbms_test.dir/adbms_code/Src/adbms_driver_v2.c.o"
+  "CMakeFiles/adbms_test.dir/adbms_code/Src/adbms_driver_v2.c.o.d"
+  "CMakeFiles/adbms_test.dir/adbms_code/Src/adbms_interface_2950.c.o"
+  "CMakeFiles/adbms_test.dir/adbms_code/Src/adbms_interface_2950.c.o.d"
+  "CMakeFiles/adbms_test.dir/adbms_code/Src/adbms_interface_6830.c.o"
+  "CMakeFiles/adbms_test.dir/adbms_code/Src/adbms_interface_6830.c.o.d"
+  "CMakeFiles/adbms_test.dir/adbms_code/Src/adbms_interface_v2.c.o"
+  "CMakeFiles/adbms_test.dir/adbms_code/Src/adbms_interface_v2.c.o.d"
+  "CMakeFiles/adbms_test.dir/adbms_code/Src/test.c.o"
+  "CMakeFiles/adbms_test.dir/adbms_code/Src/test.c.o.d"
+  "adbms_test"
+  "adbms_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/adbms_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
